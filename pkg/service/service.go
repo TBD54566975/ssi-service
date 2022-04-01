77101e6680f05e1032b1bb62c681739d82737f79
Gabe Cohen <gcohen@squareup.com>
2022-04-01 15:54:03 -0700

    renames

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -8,11 +8,14 @@ import (
 	"log"
 )
 
-// VerifiableCredentialsService is the total representation of this service, including transports and business logic
+// VerifiableCredentialsService represents all services and their dependencies independent of transport
 type VerifiableCredentialsService struct {
 	services []framework.Service
 }
 
+// NewVerifiableCredentialsService creates a new instance of the VCS which instantiates all services and their
+// dependencies independent of transport.
+// TODO(gabe) make service loading config-based
 func NewVerifiableCredentialsService(log *log.Logger) (*VerifiableCredentialsService, error) {
 	services, err := instantiateServices(log)
 	if err != nil {
@@ -27,7 +30,6 @@ func (vcs *VerifiableCredentialsService) GetServices() []framework.Service {
 	return vcs.services
 }
 
-// TODO(gabe) make this configurable
 // instantiateServices begins all instantiates and their dependencies
 func instantiateServices(log *log.Logger) ([]framework.Service, error) {
 	bolt, err := storage.NewBoltDB()
