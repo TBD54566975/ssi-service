4191b0c5c918658a4f32d2942819665eaaa76155
Gabe Cohen <gcohen@squareup.com>
2022-05-09 12:53:11 -0700

    revert

--- pkg/server/framework/errors.go
+++ pkg/server/framework/errors.go
@@ -25,7 +25,7 @@ type SafeError struct {
 }
 
 // SafeError implements the `error` interface. It uses the default message of the
-// wrapped error. This is what will be shown in a server's log
+// wrapped error. This is what will be shown in a server's logs
 func (err *SafeError) Error() string {
 	return err.Err.Error()
 }
