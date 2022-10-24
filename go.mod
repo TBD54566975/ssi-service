08e4525904ecf3284e93b330b2556059614f2b9b
nitro-neal <groessler24@gmail.com>
2022-10-24 17:21:55 -0500

    Test/updated input e2e (#144)
    * updating inputs and outputs
    
    * better formatting
    
    * valid vc output
    
    * better output
    
    * updating and cleaning up
    
    * fix build
    
    * updated comments
    
    Co-authored-by: Neal Roessler <nealr@nealr-macbookpro.local>

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/tbd54566975/ssi-service
 go 1.19
 
 require (
-	github.com/BurntSushi/toml v1.2.1
+	github.com/BurntSushi/toml v1.2.0
 	github.com/TBD54566975/ssi-sdk v0.0.1-alpha.0.20221019200812-2e73f0ac7722
 	github.com/ardanlabs/conf v1.5.0
 	github.com/dimfeld/httptreemux/v5 v5.4.0
@@ -21,17 +21,19 @@ require (
 	github.com/sirupsen/logrus v1.9.0
 	github.com/stretchr/testify v1.8.0
 	go.etcd.io/bbolt v1.3.6
-	go.opentelemetry.io/otel v1.11.1
-	go.opentelemetry.io/otel/exporters/jaeger v1.11.1
-	go.opentelemetry.io/otel/sdk v1.11.1
-	go.opentelemetry.io/otel/trace v1.11.1
+	go.opentelemetry.io/otel v1.11.0
+	go.opentelemetry.io/otel/exporters/jaeger v1.11.0
+	go.opentelemetry.io/otel/sdk v1.11.0
+	go.opentelemetry.io/otel/trace v1.11.0
 	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b
 	gopkg.in/go-playground/validator.v9 v9.31.0
 )
 
 require (
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/flowstack-com/jsonschema v0.1.2 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
@@ -51,6 +53,7 @@ require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20221006211917-84dc82d7e875 // indirect
 	golang.org/x/term v0.0.0-20220919170432-7a66f970e087 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- test/steelthread.go
+++ test/steelthread.go
@@ -4,10 +4,16 @@ import (
 	"bytes"
 	"embed"
 	"fmt"
+	manifestsdk "github.com/TBD54566975/ssi-sdk/credential/manifest"
+	"github.com/TBD54566975/ssi-sdk/crypto"
+	"github.com/mr-tron/base58"
+	credmodel "github.com/tbd54566975/ssi-service/internal/credential"
+	"github.com/tbd54566975/ssi-service/internal/keyaccess"
 	"io"
 	"net/http"
 	"strings"
 
+	cmpact "encoding/json"
 	"github.com/goccy/go-json"
 	"github.com/oliveagle/jsonpath"
 	"github.com/pkg/errors"
@@ -32,8 +38,6 @@ func RunTest() error {
 		return errors.Wrapf(err, "problem with readiness endpoint with output: %s", output)
 	}
 
-	fmt.Println(output)
-
 	// Create a did for the issuer
 	fmt.Println("\n\nCreate a did for the issuer:")
 	output, err = put(endpoint+version+"dids/key", getJSONFromFile("did-input.json"))
@@ -41,11 +45,24 @@ func RunTest() error {
 		return errors.Wrapf(err, "problem with dids/key endpoint with output: %s", output)
 	}
 
-	fmt.Println(output)
 	issuerDID, err := getJSONElement(output, "$.did.id")
 	if err != nil {
 		return errors.Wrap(err, "problem with getting json element")
 	}
+
+	// Create a did for alice
+	fmt.Println("\n\nCreate a did for alice:")
+	output, err = put(endpoint+version+"dids/key", getJSONFromFile("did-input.json"))
+	if err != nil {
+		return errors.Wrapf(err, "problem with dids/key endpoint with output: %s", output)
+	}
+
+	aliceDID, err := getJSONElement(output, "$.did.id")
+	if err != nil {
+		return errors.Wrap(err, "problem with getting json element")
+	}
+
+	aliceDIDPrivateKey, err := getJSONElement(output, "$.privateKeyBase58")
 
 	// Create a schema to be used in CM
 	fmt.Println("\n\nCreate a schema to be used in CM:")
@@ -54,24 +71,22 @@ func RunTest() error {
 		return errors.Wrapf(err, "problem with schema endpoint with output: %s", output)
 	}
 
-	fmt.Println(output)
 	schemaID, err := getJSONElement(output, "$.id")
 	if err != nil {
 		return errors.Wrap(err, "problem getting json element")
 	}
 
-	// Create a credential
+	// Create a credential to be used in CA:
 	fmt.Println("\n\nCreate a credential to be used in CA:")
 	credentialJSON := getJSONFromFile("credential-input.json")
-	credentialJSON = strings.Replace(credentialJSON, "<CREDISSUERID>", issuerDID, -1)
-	credentialJSON = strings.Replace(credentialJSON, "<CREDSUBJECTID>", issuerDID, -1)
-	credentialJSON = strings.Replace(credentialJSON, "<SCHEMAID>", schemaID, -1)
+	credentialJSON = strings.ReplaceAll(credentialJSON, "<CREDISSUERID>", issuerDID)
+	credentialJSON = strings.ReplaceAll(credentialJSON, "<CREDSUBJECTID>", issuerDID)
+	credentialJSON = strings.ReplaceAll(credentialJSON, "<SCHEMAID>", schemaID)
 	output, err = put(endpoint+version+"credentials", credentialJSON)
 	if err != nil {
 		return errors.Wrapf(err, "problem with credentials endpoint with output: %s", output)
 	}
 
-	fmt.Println(output)
 	credentialJWT, err := getJSONElement(output, "$.credentialJwt")
 	if err != nil {
 		return errors.Wrap(err, "problem getting json element")
@@ -80,15 +95,18 @@ func RunTest() error {
 	// Create our Credential Manifest
 	fmt.Println("\n\nCreate our Credential Manifest:")
 	manifestJSON := getJSONFromFile("manifest-input.json")
-	manifestJSON = strings.Replace(manifestJSON, "<SCHEMAID>", schemaID, -1)
-	manifestJSON = strings.Replace(manifestJSON, "<ISSUERID>", issuerDID, -1)
+	manifestJSON = strings.ReplaceAll(manifestJSON, "<SCHEMAID>", schemaID)
+	manifestJSON = strings.ReplaceAll(manifestJSON, "<ISSUERID>", issuerDID)
 	output, err = put(endpoint+version+"manifests", manifestJSON)
 	if err != nil {
 		return errors.Wrapf(err, "problem with manifest endpoint with output: %s", output)
 	}
 
-	fmt.Println(output)
 	presentationDefinitionID, err := getJSONElement(output, "$.credential_manifest.presentation_definition.id")
+	if err != nil {
+		return errors.Wrap(err, "problem getting json element")
+	}
+	manifestID, err := getJSONElement(output, "$.credential_manifest.id")
 	if err != nil {
 		return errors.Wrap(err, "problem getting json element")
 	}
@@ -96,19 +114,59 @@ func RunTest() error {
 	// Submit an application
 	fmt.Println("\n\nSubmit an Application:")
 	applicationJSON := getJSONFromFile("application-input.json")
-	applicationJSON = strings.Replace(applicationJSON, "<DEFINITIONID>", presentationDefinitionID, -1)
-	applicationJSON = strings.Replace(applicationJSON, "<VCJWT>", credentialJWT, -1)
+	applicationJSON = strings.ReplaceAll(applicationJSON, "<DEFINITIONID>", presentationDefinitionID)
+	applicationJSON = strings.ReplaceAll(applicationJSON, "<VCJWT>", credentialJWT)
+	applicationJSON = strings.ReplaceAll(applicationJSON, "<MANIFESTID>", manifestID)
 
-	// sign the application as a jwt
+	// Start signing credential application
+	alicPrivKeyBytes, err := base58.Decode(aliceDIDPrivateKey)
+	if err != nil {
+		return errors.Wrap(err, "problem base58 decoding")
+	}
 
-	output, err = put(endpoint+version+"manifests/applications", applicationJSON)
+	alicePrivKey, err := crypto.BytesToPrivKey(alicPrivKeyBytes, crypto.Ed25519)
+	if err != nil {
+		return errors.Wrap(err, "problem with bytes to priv key")
+	}
+
+	signer, err := keyaccess.NewJWKKeyAccess(aliceDID, alicePrivKey)
+	if err != nil {
+		return errors.Wrap(err, "problem with creating signer")
+	}
+
+	credAppWrapper := getValidApplicationRequest(applicationJSON, credentialJWT)
+
+	signed, err := signer.SignJSON(credAppWrapper)
+	if err != nil {
+		return errors.Wrap(err, "problem signing json")
+	}
+
+	fmt.Println("\nApplication JSON:")
+	fmt.Println(compactJSONOutput(applicationJSON))
+
+	fmt.Println("\nSIGNED APPLICATION JWT:")
+	fmt.Println(signed)
+	// End signing credential application
+
+	trueApplicationJSON := getJSONFromFile("application-input-jwt.json")
+	trueApplicationJSON = strings.Replace(trueApplicationJSON, "<APPLICATIONJWT>", signed.String(), -1)
+
+	output, err = put(endpoint+version+"manifests/applications", trueApplicationJSON)
 	if err != nil {
 		return errors.Wrapf(err, "problem with application endpoint with output: %s", output)
 	}
 
-	fmt.Println(output)
-
 	return err
+}
+
+func compactJSONOutput(json string) string {
+	jsonBytes := []byte(json)
+	buffer := new(bytes.Buffer)
+	if err := cmpact.Compact(buffer, jsonBytes); err != nil {
+		fmt.Println(err)
+	}
+
+	return buffer.String()
 }
 
 func getJSONElement(jsonString string, jsonPath string) (string, error) {
@@ -126,6 +184,8 @@ func getJSONElement(jsonString string, jsonPath string) (string, error) {
 }
 
 func get(url string) (string, error) {
+	fmt.Printf("\nPerforming GET request to:  %s\n", url)
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", errors.Wrap(err, "problem with finding element in json string")
@@ -140,10 +200,15 @@ func get(url string) (string, error) {
 		return "", fmt.Errorf("status code not in the 200s. body: %s", string(body))
 	}
 
+	fmt.Printf("\nOutput:\n")
+	fmt.Println(string(body))
+
 	return string(body), err
 }
 
 func put(url string, json string) (string, error) {
+	fmt.Printf("\nPerforming PUT request to:  %s \n\nwith data: \n%s\n", url, compactJSONOutput(json))
+
 	client := new(http.Client)
 
 	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer([]byte(json)))
@@ -166,6 +231,9 @@ func put(url string, json string) (string, error) {
 		return "", fmt.Errorf("status code not in the 200s. body: %s", string(body))
 	}
 
+	fmt.Printf("\nOutput:\n")
+	fmt.Println(string(body))
+
 	return string(body), err
 }
 
@@ -176,4 +244,23 @@ func getJSONFromFile(fileName string) string {
 
 func is200Response(statusCode int) bool {
 	return statusCode/100 != 2
+}
+
+func getValidApplicationRequest(credAppJSON string, credentialJWT string) manifestsdk.CredentialApplicationWrapper {
+	var createApplication manifestsdk.CredentialApplication
+	if err := json.Unmarshal([]byte(credAppJSON), &createApplication); err != nil {
+		fmt.Println("unmarshal error")
+	}
+
+	contain, err := credmodel.NewCredentialContainerFromJWT(credentialJWT)
+	if err != nil {
+		fmt.Println("Problem making NewCredentialContainerFromJWT")
+	}
+	contains := []credmodel.Container{*contain}
+
+	creds := credmodel.ContainersToInterface(contains)
+	return manifestsdk.CredentialApplicationWrapper{
+		CredentialApplication: createApplication,
+		Credentials:           creds,
+	}
 }
