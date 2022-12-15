2614396a766ecd4878d120d98b332a821579f07f
suutaku <johnsuu@cotnetwork.com>
2022-12-15 10:43:20 +0800

    modity storage

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -10,25 +10,45 @@ const (
 	Bolt Storage = "bolt"
 )
 
+var (
+	availableStorages = make(map[Storage]ServiceStorage)
+)
+
 // ServiceStorage describes the api for storage independent of DB providers
 type ServiceStorage interface {
+	Init(interface{}) error
 	Type() Storage
+	Uri() string
+	IsOpen() bool
 	Close() error
 	Write(namespace, key string, value []byte) error
 	Read(namespace, key string) ([]byte, error)
 	ReadAll(namespace string) (map[string][]byte, error)
+	ReadPrefix(namespace, prefix string) (map[string][]byte, error)
+	ReadAllKeys(namespace string) ([]string, error)
 	Delete(namespace, key string) error
 	DeleteNamespace(namespace string) error
+	Update(namespace string, key string, values map[string]any) ([]byte, error)
+	UpdateValueAndOperation(namespace, key string, updater Updater, opNamespace, opKey string, opUpdater ResponseSettingUpdater) (first, op []byte, err error)
 }
 
-// NewStorage creates a new storage provider based on the input
-func NewStorage(storageProvider Storage) (ServiceStorage, error) {
-	switch storageProvider {
-	case Bolt:
-		return NewBoltDB()
-	default:
-		return nil, fmt.Errorf("unsupported storage provider: %s", storageProvider)
+//  NewStorage creates a new or use exists storage provider and provider specify param based on the input
+func NewStorage(storageProvider Storage, option interface{}) (ServiceStorage, error) {
+	impl := GetStorage(storageProvider)
+	if impl == nil {
+		return impl, fmt.Errorf("unsupported storage provider: %s", storageProvider)
 	}
+	err := impl.Init(option)
+	return impl, err
+}
+
+// RegisterStorage resister a storage dynamically
+func RegisterStorage(storage ServiceStorage) error {
+	if availableStorages[storage.Type()] != nil {
+		return fmt.Errorf("storage implementation for %s already exists", storage.Type())
+	}
+	availableStorages[storage.Type()] = storage
+	return nil
 }
 
 // AvailableStorage returns the supported storage providers
@@ -45,4 +65,9 @@ func IsStorageAvailable(storage string) bool {
 		}
 	}
 	return false
+}
+
+// GetStorage get storage by stroage type
+func GetStorage(storageType Storage) ServiceStorage {
+	return availableStorages[storageType]
 }
