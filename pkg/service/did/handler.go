38270d4d7f2f96e7379f27c95f4ddceb4316fd43
gabe <gcohen@squareup.com>
2023-03-28 12:22:11 -0700

    method handler for resolver

--- pkg/service/did/handler.go
+++ pkg/service/did/handler.go
@@ -6,8 +6,6 @@ import (
 	didsdk "github.com/TBD54566975/ssi-sdk/did"
 	"github.com/TBD54566975/ssi-sdk/util"
 	"github.com/pkg/errors"
-
-	utilint "github.com/tbd54566975/ssi-service/internal/util"
 )
 
 // MethodHandler describes the functionality of *all* possible DID service, regardless of method
@@ -18,9 +16,10 @@ type MethodHandler interface {
 	SoftDeleteDID(ctx context.Context, request DeleteDIDRequest) error
 }
 
-// HandlerResolver is a DID resolver that uses MethodHandlers to resolve DIDs, meaning DIDs we store in our database
+// HandlerResolver is a DID resolver that uses a multimethod resolver backed by method handlers to resolve DIDs,
+// meaning DIDs we store in our database
 type HandlerResolver struct {
-	handlers map[didsdk.Method]MethodHandler
+	*didsdk.MultiMethodResolver
 }
 
 // NewHandlerResolver creates a new HandlerResolver from a map of MethodHandlers which are used to resolve DIDs
@@ -28,31 +27,60 @@ func NewHandlerResolver(handlers map[didsdk.Method]MethodHandler) (*HandlerResol
 	if len(handlers) == 0 {
 		return nil, util.LoggingNewError("no handlers provided")
 	}
-	return &HandlerResolver{handlers: handlers}, nil
+
+	methodResolvers := make([]didsdk.Resolver, 0, len(handlers))
+	for method, handler := range handlers {
+		methodResolver := resolverFromHandler(method, handler)
+		methodResolvers = append(methodResolvers, methodResolver)
+	}
+
+	multiMethodResolver, err := didsdk.NewResolver(methodResolvers...)
+	if err != nil {
+		return nil, errors.Wrap(err, "creating multi-method resolver")
+	}
+
+	return &HandlerResolver{MultiMethodResolver: multiMethodResolver}, nil
 }
 
-// Resolve resolves a DID using the MethodHandler for the DID's method, wrapping the result in a ResolutionResult
-func (hr HandlerResolver) Resolve(ctx context.Context, did string, _ ...didsdk.ResolutionOption) (*didsdk.ResolutionResult, error) {
-	// get method from DID
-	method, err := utilint.GetMethodForDID(did)
+// Resolve resolves a DID using the embedded MultiMethodResolver
+func (hr HandlerResolver) Resolve(ctx context.Context, did string, opts ...didsdk.ResolutionOption) (*didsdk.ResolutionResult, error) {
+	return hr.MultiMethodResolver.Resolve(ctx, did, opts)
+}
+
+func (hr HandlerResolver) Methods() []didsdk.Method {
+	return hr.MultiMethodResolver.Methods()
+}
+
+// handlerResolver is a DID resolver that uses a MethodHandler to resolve DIDs
+type handlerResolver struct {
+	handler MethodHandler
+	method  didsdk.Method
+}
+
+func (h handlerResolver) Resolve(ctx context.Context, did string, _ ...didsdk.ResolutionOption) (*didsdk.ResolutionResult, error) {
+	method, err := didsdk.GetMethodForDID(did)
 	if err != nil {
 		return nil, errors.Wrap(err, "getting method from DID")
 	}
 
-	if handler, ok := hr.handlers[method]; ok {
-		gotDID, err := handler.GetDID(ctx, GetDIDRequest{Method: method, ID: did})
-		if err != nil {
-			return nil, errors.Wrap(err, "getting DID from handler")
-		}
-		return &didsdk.ResolutionResult{Document: gotDID.DID}, nil
+	if method != h.method {
+		return nil, errors.Errorf("invalid method %s for handler %s", method, h.method)
 	}
-	return nil, util.LoggingNewErrorf("no handler for method %s", method)
+
+	gotDIDResponse, err := h.handler.GetDID(ctx, GetDIDRequest{
+		Method: h.method,
+		ID:     did,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "getting DID from handler")
+	}
+	return &didsdk.ResolutionResult{Document: gotDIDResponse.DID}, nil
 }
 
-func (hr HandlerResolver) Methods() []didsdk.Method {
-	methods := make([]didsdk.Method, 0, len(hr.handlers))
-	for method := range hr.handlers {
-		methods = append(methods, method)
-	}
-	return methods
+func (h handlerResolver) Methods() []didsdk.Method {
+	return []didsdk.Method{h.method}
+}
+
+func resolverFromHandler(method didsdk.Method, handler MethodHandler) didsdk.Resolver {
+	return &handlerResolver{handler: handler, method: method}
 }
