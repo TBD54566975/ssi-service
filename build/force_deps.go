1b49ab5849a568508d625658852eabb3177714b8
Andres Uribe Gonzalez <auribe@squareup.com>
2023-06-09 19:25:05 -0400

    lint

--- build/force_deps.go
+++ build/force_deps.go
@@ -1,5 +1,7 @@
 package build
 
-// This is here to force `go.mod` to pick up the github.com/btcsuite/btcd/chaincfg/chainhash dependency. It is a
-// workaround to fix https://github.com/TBD54566975/ssi-service/issues/483
-import _ "github.com/btcsuite/btcd/chaincfg/chainhash"
+import (
+	// This is here to force `go.mod` to pick up the github.com/btcsuite/btcd/chaincfg/chainhash dependency. It is a
+	// workaround to fix https://github.com/TBD54566975/ssi-service/issues/483
+	_ "github.com/btcsuite/btcd/chaincfg/chainhash"
+)
