badc1ff8433ef9313c501cd14735d1dc70e3cb82
Andres Uribe Gonzalez <auribe@squareup.com>
2023-08-11 13:57:27 -0400

    Missed one bit

--- pkg/service/did/model.go
+++ pkg/service/did/model.go
@@ -100,14 +100,6 @@ type UpdateIONDIDResponse struct {
 	DID didsdk.Document `json:"did"`
 }
 
-type DeactivateIONDIDRequest struct {
-	DID ion.ION `json:"did"`
-}
-
-type DeactivateIONDIDResponse struct {
-	Result *resolution.Result `json:"resolutionResult"`
-}
-
 type UpdateRequestStatus string
 
 func (s UpdateRequestStatus) Bytes() []byte {
